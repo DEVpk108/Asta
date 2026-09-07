8d46e52898087e8412e530fb873644af6a6757c3
Prasant <115569316+DEVpk108@users.noreply.github.com>
2026-09-07 22:35:11 +0530

    Export complete built-in tool set

--- core/tools/__init__.py
+++ core/tools/__init__.py
@@ -1,5 +1,14 @@
 from .approval import ApprovalManager, PendingApproval
 from .base import Tool
+from .builtin import (
+    AudioControlTool,
+    CloseApplicationTool,
+    LaunchApplicationTool,
+    RunCommandTool,
+    ScreenshotTool,
+    StartProcessTool,
+    StopProcessTool,
+)
 from .dispatcher import ToolDispatcher
 from .module import ToolRuntimeModule
 from .policy import (
@@ -14,17 +23,9 @@ from .system import OpenApplicationTool
 from .test_tool import EchoTool
 
 __all__ = [
-    "ApprovalManager",
-    "PendingApproval",
-    "AuthorizationResult",
-    "AuthorityPolicy",
-    "RiskLevel",
-    "Tool",
-    "ToolDispatcher",
-    "ToolRuntimeModule",
-    "ToolRegistry",
-    "ToolRequestBuilder",
-    "ToolSelector",
-    "OpenApplicationTool",
-    "EchoTool",
+    "ApprovalManager", "PendingApproval", "AuthorizationResult", "AuthorityPolicy",
+    "RiskLevel", "Tool", "ToolDispatcher", "ToolRuntimeModule", "ToolRegistry",
+    "ToolRequestBuilder", "ToolSelector", "OpenApplicationTool", "LaunchApplicationTool",
+    "StartProcessTool", "RunCommandTool", "StopProcessTool", "CloseApplicationTool",
+    "ScreenshotTool", "AudioControlTool", "EchoTool",
 ]
