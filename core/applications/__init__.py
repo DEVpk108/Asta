d0270dcfcdedac7cbe2e3e3f6eed717fc31e6323
Prasant <115569316+DEVpk108@users.noreply.github.com>
2026-09-20 02:07:23 +0530

    feat: export running process records

--- core/applications/__init__.py
+++ core/applications/__init__.py
@@ -1,11 +1,13 @@
 from .manager import (
     ApplicationManager,
     ApplicationRecord,
+    RunningProcessRecord,
     ApplicationResolutionError,
 )
 
 __all__ = [
     "ApplicationManager",
     "ApplicationRecord",
+    "RunningProcessRecord",
     "ApplicationResolutionError",
 ]
