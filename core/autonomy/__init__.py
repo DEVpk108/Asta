3e89eaa48065dbc55cd2f36ba4537afb71e70737
Prasant <115569316+DEVpk108@users.noreply.github.com>
2026-09-25 23:30:13 +0530

    feat: export autonomy diagnosis contracts

--- core/autonomy/__init__.py
+++ core/autonomy/__init__.py
@@ -1,8 +1,12 @@
 """Autonomous task orchestration primitives for A.S.T.A."""
 
+from .diagnosis import DiagnosisCategory, DiagnosisEngine, FailureDiagnosis
 from .recovery import RecoveryAction, RecoveryDecision, RecoveryManager
 
 __all__ = [
+    "DiagnosisCategory",
+    "DiagnosisEngine",
+    "FailureDiagnosis",
     "RecoveryAction",
     "RecoveryDecision",
     "RecoveryManager",
