7f56658d0aa8bc865d87ed23d0b8f8b6c3950a40
Prasant <115569316+DEVpk108@users.noreply.github.com>
2026-09-25 09:30:53 +0530

    feat: register recovery manager in kernel

--- core/kernel.py
+++ core/kernel.py
@@ -1,6 +1,7 @@
 import threading
 
 from .applications import ApplicationManager
+from .autonomy import RecoveryManager
 from .capability_discovery import CapabilityDiscovery
 from .decision import create_decision_engine
 from .event_bus import EventBus
@@ -36,6 +37,7 @@ class Kernel:
         )
         self.decision_engine = create_decision_engine()
         self.task_manager = TaskManager(event_bus=self.event_bus)
+        self.recovery_manager = RecoveryManager()
         self.workspace_manager = WorkspaceManager(event_bus=self.event_bus)
         self.application_manager = ApplicationManager()
         self.skill_manager = SkillManager(event_bus=self.event_bus)
